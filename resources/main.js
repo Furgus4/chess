b9f81894287d307e1d14ac07b3cc5f98437dcd28
Furgus4 <ryantomich80@gmail.com>
2024-02-27 10:44:24 -0500

    add piece assets and render with css instead of canvas

--- resources/main.js
+++ resources/main.js
@@ -1,22 +1,5 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+init(); // document.onload(init());
 
-canvas.width = 1080;
-canvas.height = 1080;
-
-const darkSquareColor = "#305E7E";
-const lightSquareColor = "#FAF6EC";
-const squareLength = canvas.width / 8;
-
-// create board
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    if (i % 2 == 0) {
-      ctx.fillStyle = j % 2 == 0 ? darkSquareColor : lightSquareColor;
-      ctx.fillRect(i*squareLength, j*squareLength, squareLength, squareLength);
-    } else {
-      ctx.fillStyle = j % 2 != 0 ? darkSquareColor : lightSquareColor;
-      ctx.fillRect(i*squareLength, j*squareLength, squareLength, squareLength);
-    }
-  }
+function init() {
+  // other stuff
 }
