33ac7f0fbd822d6cb10f921106db5e71a28228a0
Furgus4 <ryantomich80@gmail.com>
2024-02-26 19:06:23 -0500

    fourth attempt

--- resources/main.js
+++ resources/main.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 405;
-canvas.height = 405;
+canvas.width = 1080;
+canvas.height = 1080;
 
 const darkSquareColor = "#305E7E";
 const lightSquareColor = "#FAF6EC";
